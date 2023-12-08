7ac4cb39be807a0e1193492bf7fa895d448d6156
TaiChi <anothai.0978452316@gmail.com>
2023-12-09 03:10:04 +0700

    start to learning shell script basic begginer

---
+++ shellTest.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+:<< 'comment'
+echo Hello Shell Script
+first_name=TaiChi
+last_name=foo_bar_baz
+echo Hello $first_name $last_name
+echo What is your fname?
+read fname
+echo What is yout lname
+read lname
+echo Hello $fname $lname
+comment
+echo Hello Shell Script
+
+echo Hello $1 $2
+
+
+Hello again
+what is your age? read age echo My age is :
