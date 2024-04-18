19895bd2d8b0d73199590580a68b4320d983c0e5
TaiChi <anothai.0978452316@gmail.com>
2024-04-19 01:11:42 +0700

    test : test declaration variable

--- shell112/main.sh
+++ shell112/main.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 # This test comment
+
+firstname="TaiChi"
+
+lastname="Soy eiei"
+
+echo "My name is : $firstname $lastname"
+
 echo "First statement to test shell script"
+
+length=${#firstname}
+echo "Length of concatenated string: $length"
