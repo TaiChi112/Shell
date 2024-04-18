410dbdeaa6dac44729be13e2bd67afe9b208715f
TaiChi <anothai.0978452316@gmail.com>
2024-04-19 01:37:36 +0700

    feat && test : soy soy spicy eiei

---
+++ shell112/logic.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+# Logical AND operator
+# num1=5
+# num2=10
+# if [$num1 -gt 0 ] && [$num2 -gt 0]; then
+#     echo "Both numbers are greater than 0"
+# fi
+
+greeting() {
+    echo "Test Test : Hello World"
+}
+
+export -f greeting
--- shell112/main.sh
+++ shell112/main.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+source logic.sh
+
+greeting
+ 
+
+
 # This test comment
 
 firstname="TaiChi"
@@ -12,3 +18,16 @@ echo "First statement to test shell script"
 
 length=${#firstname}
 echo "Length of concatenated string: $length"
+
+# Addition
+
+result=$((2 + 2))
+echo "Result: $result"
+
+result=$((5 - 6))
+echo "Result: $result"
+
+num1=10
+num2=20
+result=$((num1 + num2))
+echo "Result: $result"
